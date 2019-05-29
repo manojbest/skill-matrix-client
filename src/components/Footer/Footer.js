0949011fdeee395b9961dafb31ca8d531b3e9d46
Bandara <mbandara@wiley.com>
2019-05-29 23:56:55 +0530

    Initial Layout Design

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import "./Footer.css";
+
+import Navbar from 'react-bootstrap/Navbar';
+
 const footer = () => (
-    <div>This is footer</div>
+    <Navbar  bg="lightgray" variant="lightgray" fixed="bottom">
+        <Navbar.Brand>Wiley © 2019</Navbar.Brand>
+    </Navbar>
 );
 
 export default footer;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
+import Navbar from 'react-bootstrap/Navbar';
+
+import './Header.css';
+
 const header = () => (
-    <div>This is header</div>
+    
+    <Navbar bg="gray" variant="dark" fixed="top">
+        <Navbar.Brand href="#home">
+            <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/01/Wiley_logo.svg"
+            width="65"
+            height="65"
+            className="d-inline-block align-top"
+            alt="Wiley logo"
+            />
+           </Navbar.Brand>
+           <Navbar.Brand href="#home">Skill Matix</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+                Logged as: <a href="#login">Manoj</a>
+            </Navbar.Text>
+        </Navbar.Collapse>
+    </Navbar>
 );
 
 export default header;
